a908d19244ebdcfa5633815808bb9d09358c5fb5
aparrett <anthonyparrett7@gmail.com>
2020-02-06 19:41:04 -0600

    Setup prettier and eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,25 +1,41 @@
 module.exports = {
-  root: true,
-  env: {
-    node: true
-  },
-  extends: ["plugin:vue/essential", "@vue/prettier"],
-  rules: {
-    "no-console": process.env.NODE_ENV === "production" ? "error" : "off",
-    "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off"
-  },
-  parserOptions: {
-    parser: "babel-eslint"
-  },
-  overrides: [
-    {
-      files: [
-        "**/__tests__/*.{j,t}s?(x)",
-        "**/tests/unit/**/*.spec.{j,t}s?(x)"
-      ],
-      env: {
-        jest: true
-      }
-    }
-  ]
-};
+    root: true,
+    env: {
+        node: true,
+        es6: true,
+        commonjs: true
+    },
+    extends: ['plugin:vue/essential', '@vue/prettier'],
+    rules: {
+        'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+        'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+        'prettier/prettier': 'error',
+        curly: ['error', 'all'],
+        'no-dupe-else-if': 'error',
+        'no-dupe-keys': 'error',
+        'no-extra-boolean-cast': 'error',
+        'no-irregular-whitespace': 'error',
+        'no-unreachable': 'error',
+        'no-unused-expressions': 'error',
+        'no-useless-catch': 'error',
+        'require-await': 'error',
+        'no-unused-vars': 'error',
+        'comma-dangle': 'error',
+        'key-spacing': 'error',
+        'prefer-const': 'error'
+    },
+    parserOptions: {
+        parser: 'babel-eslint'
+    },
+    overrides: [
+        {
+            files: [
+                '**/__tests__/*.{j,t}s?(x)',
+                '**/tests/unit/**/*.spec.{j,t}s?(x)'
+            ],
+            env: {
+                jest: true
+            }
+        }
+    ]
+}
